7d11beea1bac88de1d4034acaeea04def681b5fe
Diana Chong <diana.chong@asti-usa.com>
2024-10-03 10:16:39 -0400

    notes finished tutorial for ajs

--- app/core/phone/phone.service.js
+++ app/core/phone/phone.service.js
@@ -13,3 +13,10 @@ angular.
       });
     }
   ]);
+// use the factory function to register a custom service
+// inputs to the function include the name of the service and the factory function
+// factory function is similar to a controller's constructor
+  // both can declare dependencies to be injected via function arguments
+  // phone service declares dependency on $resource service
+  // use $resource service to create a RESTful client
+  // use client instead of $http service
